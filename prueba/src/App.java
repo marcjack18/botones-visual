10792dafca7d1f80365d34334ba7795820cf6bfd
marcospallas <marcospp172006@gmail.com>
2025-02-05 13:02:48 +0100

    Update App.java

--- prueba/src/App.java
+++ prueba/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int numero=0;
-        int dos=0;
+        int tres=0;
     }
 }
